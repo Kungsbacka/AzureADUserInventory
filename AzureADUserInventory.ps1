d546a1d53454517d355399d4a6cb5be2aece8c6f
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2022-10-12 22:20:54 +0200

    Initial commit

---
+++ AzureADUserInventory.ps1
@@ -0,0 +1,116 @@
+$ErrorActionPreference = 'Stop'
+
+. "$PSScriptRoot\Config.ps1"
+$certPassword = $Script:Config.CertificatePassword | ConvertTo-SecureString
+$cert =  [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($Script:Config.CertificatePath, $certPassword)
+Connect-MgGraph -ClientId $Script:Config.ClientId -Certificate $cert -TenantId $Script:Config.TenantId
+
+$properties = @(
+    'Id'
+    'CreatedDateTime'
+    'LicenseAssignmentStates'
+    'AssignedLicenses'
+    'OnPremisesImmutableId'
+    'OnPremisesLastSyncDateTime'
+    'OnPremisesProvisioningErrors'
+    'OnPremisesSyncEnabled'
+    'UserPrincipalName'
+    'AccountEnabled'
+    'UserType'
+)
+
+$userTable = New-Object -TypeName 'System.Data.DataTable' -ArgumentList @('AzureADUser')
+[void]$userTable.Columns.Add('id', 'guid')
+[void]$userTable.Columns.Add('createdDateTime', 'datetime')
+[void]$userTable.Columns.Add('accountEnabled', 'boolean')
+[void]$userTable.Columns.Add('onPremisesImmutableId', 'guid')
+[void]$userTable.Columns.Add('onPremisesSyncEnabled', 'boolean')
+[void]$userTable.Columns.Add('onPremisesLastSyncDateTime', 'datetime')
+[void]$userTable.Columns.Add('onPremisesProvisioningErrors', 'boolean')
+[void]$userTable.Columns.Add('userPrincipalName', 'string')
+
+$licenseTable = New-Object -TypeName 'System.Data.DataTable' -ArgumentList @('AzureADAssignedLicense')
+[void]$licenseTable.Columns.Add('id', 'int')
+[void]$licenseTable.Columns.Add('userId', 'guid')
+[void]$licenseTable.Columns.Add('skuId', 'guid')
+[void]$licenseTable.Columns.Add('error', 'boolean')
+[void]$licenseTable.Columns.Add('state', 'string')
+[void]$licenseTable.Columns.Add('lastUpdatedDateTime', 'datetime')
+[void]$licenseTable.Columns.Add('assignedByGroup', 'guid')
+
+
+function ValueOrNull($value) {
+    if ($value) {
+        $value
+    }
+    else {
+        [System.DBNull]::Value
+    }
+}
+
+function ToBool($value) {
+    if ($value) {
+        $true
+    }
+    else {
+        $false
+    }
+}
+
+foreach ($user in (Get-MgUser -Filter "userType eq 'Member'" -All -Property $properties)) {
+    $onPremisesImmutableId = $null
+    if ($user.OnPremisesImmutableId) {
+        $bytes = [Convert]::FromBase64String($user.OnPremisesImmutableId)
+        if ($bytes.Length -eq 16) {
+            $onPremisesImmutableId = [Guid]::new($bytes)
+        }
+    }
+    $row = $userTable.NewRow()
+    $row['id'] = $user.Id
+    $row['createdDateTime'] = $user.CreatedDateTime
+    $row['accountEnabled'] = $user.AccountEnabled
+    $row['onPremisesImmutableId'] = ValueOrNull $onPremisesImmutableId
+    $row['onPremisesSyncEnabled'] = ToBool $user.OnPremisesSyncEnabled
+    $row['onPremisesLastSyncDateTime'] = ValueOrNull $user.OnPremisesLastSyncDateTime
+    $row['onPremisesProvisioningErrors'] = ($user.OnPremisesProvisioningErrors.Count -eq 0)
+    $row['userPrincipalName'] = $user.UserPrincipalName
+    $userTable.Rows.Add($row)
+    
+    foreach($license in $user.LicenseAssignmentStates) {
+        $row = $licenseTable.NewRow()
+        $row['userId'] = $user.Id
+        $row['skuId'] = $license.SkuId
+        $row['error'] = ($license.Error -ne 'None')
+        $row['state'] = ValueOrNull $license.State
+        $row['lastUpdatedDateTime'] = ValueOrNull $license.LastUpdatedDateTime
+        $row['assignedByGroup'] = ValueOrNull $license.AssignedByGroup
+        $licenseTable.Rows.Add($row)
+    }
+}
+
+$conn = New-Object -TypeName 'System.Data.SqlClient.SqlConnection'
+$conn.ConnectionString = $Script:Config.ConnectionString
+$conn.Open()
+$cmd = New-Object -TypeName 'System.Data.SqlClient.SqlCommand'
+$cmd.Connection = $conn
+$cmd.CommandType = 'Text'
+
+$cmd.CommandText = 'TRUNCATE TABLE dbo.AzureADUser'
+[void]$cmd.ExecuteNonQuery()
+
+$cmd.CommandText = 'TRUNCATE TABLE dbo.AzureADAssignedLicense'
+[void]$cmd.ExecuteNonQuery()
+
+$cmd.Dispose()
+
+$bulkCopy = New-Object -TypeName 'System.Data.SqlClient.SqlBulkCopy' -ArgumentList @($conn)
+$bulkCopy.DestinationTableName = 'AzureADUser'
+$bulkCopy.WriteToServer($userTable)
+$bulkCopy.Dispose()
+
+$bulkCopy = New-Object -TypeName 'System.Data.SqlClient.SqlBulkCopy' -ArgumentList @($conn)
+$bulkCopy.DestinationTableName = 'AzureADAssignedLicense'
+$bulkCopy.WriteToServer($licenseTable)
+$bulkCopy.Dispose()
+
+$conn.Dispose()
---
+++ Config.example.ps1
@@ -0,0 +1,7 @@
+$Script:Config = @{
+    TenantId = '<Your tenant ID>'
+    ClientId = '<Service Principal Client ID>'
+    CertificatePath = '<Path to certificate file>'
+    CertificatePassword = '<Certificate password as secure string>'
+    ConnectionString = '<Destination database connection string>'
+}
